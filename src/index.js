dbf15efc6a99bb8edb546ccde3a1325d67679c01
David <gooster.snesi@gmail.com>
2016-11-29 21:00:33 +0000

    Fix global css imports

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import {render} from 'react-dom';
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
 /* eslint-disable import/no-unresolved */
-import '!style!css!./styles/styles.css';
-import '!style!css!../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 
 render (
